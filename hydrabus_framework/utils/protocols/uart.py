8045aec9a5945ed3dc232d13c92fec6e3d069b03
ghecko <ovre.jordan@gmail.com>
2019-07-26 10:30:29 +0200

    Change function return

--- hydrabus_framework/utils/protocols/uart.py
+++ hydrabus_framework/utils/protocols/uart.py
@@ -1,3 +1,4 @@
+from hydrabus_framework.core.logger import Logger
 from hydrabus_framework.utils.generic import *
 
 
@@ -6,17 +7,21 @@ def hb_set_baudrate(serial_instance, baudrate):
     Set the desired baudrate value to communicate with the target device.
     :param serial_instance: hydrabus serial instance
     :param baudrate: dictionnary containing desired baudrate speed in hexadecimal 'hex' and decimal 'dec' value
-    :return: Bool, string msg
+    :return: Bool
     """
+    logger = Logger()
     hb_reset(serial_instance)
     if hb_init_uart(serial_instance):
         # Change baudrate speed
         serial_instance.write(baudrate["hex"])
         if b'\x01' != serial_instance.read(1):
-            return False, "Failed to change baudrate"
-        return True, "Switching to baudrate: {}".format(baudrate["dec"])
+            logger.print("Failed to change baudrate", "error")
+            return False
+        logger.print("Switching to baudrate: {}".format(baudrate["dec"]), "success")
+        return True
     else:
-        return False, "Failed to init BBIO_UART mode"
+        logger.print("Failed to init BBIO_UART mode", "error")
+        return False
 
 
 def hb_switch_uart(serial_instance):
@@ -25,8 +30,10 @@ def hb_switch_uart(serial_instance):
     :param serial_instance: hydrabus serial instance
     :return: Bool
     """
+    logger = Logger()
     serial_instance.write(b'\x03')
     if "ART1".encode('utf-8') not in serial_instance.read(4):
+        logger.print("Cannot enter UART mode", "error")
         return False
     return True
 
