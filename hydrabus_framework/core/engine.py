c794f917baafc8fb293fe67c904572e2f09cede3
ghecko <ovre.jordan@gmail.com>
2019-08-07 02:38:48 +0200

    Autocompletion first version

--- hydrabus_framework/core/engine.py
+++ hydrabus_framework/core/engine.py
@@ -9,6 +9,7 @@ from prompt_toolkit.styles import Style
 from hydrabus_framework.core.Dispatcher import Dispatcher
 from hydrabus_framework.modules.AModule import AModule
 from hydrabus_framework.utils.logger import Logger
+from hydrabus_framework.utils.NestedCompleter import NestedCompleter
 
 
 __author__ = "Jordan Ovrè <ghecko78@gmail.com>"
@@ -23,9 +24,10 @@ class HydraFramework:
         self.app_path = sys.path[0]
         self.current_module = None
         self.current_module_name = None
+        self.dispatcher = Dispatcher()
+        self.console_completer = NestedCompleter(self.simple_cmd_dict(), ignore_case=True)
         self.modules = self._list_modules()
         self.modules_history = []
-        self.dispatcher = Dispatcher()
         self.prompt_style = Style.from_dict({
             # User input (default text).
             '': '#ffffff',
@@ -86,9 +88,16 @@ class HydraFramework:
                     if inspect.isclass(obj) and issubclass(obj, AModule) and obj is not AModule:
                         module_path = module.replace('hbfmodules.', '').replace('.', '/')
                         modules.append({"path": module_path, "class": obj})
+                        self.console_completer.words_dic["use"].append(module_path)
             except ImportError:
                 self.logger.handle('Error dynamically import package "{}"...'.format(module), Logger.ERROR)
         return modules
+
+    def simple_cmd_dict(self):
+        commands = {}
+        for command in self.dispatcher.commands:
+            commands.update({command["name"]: []})
+        return commands
 
     def run(self):
         """
@@ -100,7 +109,8 @@ class HydraFramework:
         session = PromptSession()
         try:
             while True:
-                command = session.prompt(self.prompt, style=self.prompt_style)
+                command = session.prompt(self.prompt, style=self.prompt_style, completer=self.console_completer,
+                                         complete_while_typing=False)
                 self.dispatcher.handler(self, command)
                 self.update_prompt()
         except KeyboardInterrupt:
---
+++ hydrabus_framework/utils/NestedCompleter.py
@@ -0,0 +1,54 @@
+from prompt_toolkit.completion import Completion, Completer
+
+
+class NestedCompleter(Completer):
+
+    def __init__(self, words_dic=None, meta_dict=None, ignore_case=True, match_middle=True):
+        if meta_dict is None:
+            meta_dict = {}
+        if words_dic is None:
+            words_dic = {}
+        self.ignore_case = ignore_case
+        self.match_middle = match_middle
+        self.words_dic = words_dic
+        self.meta_dict = meta_dict
+        pass
+
+    def get_completions(self, document, complete_event):
+        text_before_cursor = document.text_before_cursor
+        if self.ignore_case:
+            text_before_cursor = text_before_cursor.lower()
+
+        text_before_cursor = str(text_before_cursor)
+        text_arr = text_before_cursor.split(' ')
+        last_words = text_arr[-1]
+        words = self.__get_current_words(text_arr[:-1])
+
+        def word_matches(word):
+            """ True when the word before the cursor matches. """
+            if self.ignore_case:
+                word = word.lower()
+
+            if self.match_middle:
+                return last_words in word
+            else:
+                return word.startswith(last_words)
+
+        for a in words:
+            if word_matches(a):
+                display_meta = self.meta_dict.get(a, '')
+                yield Completion(a, -len(last_words), display_meta=display_meta)
+
+    def __get_current_words(self, text_arr):
+        current_dic = self.words_dic
+        for tmp in text_arr:
+            if tmp == ' ' or tmp == '':
+                continue
+            try:
+                if tmp in current_dic.keys():
+                    current_dic = current_dic[tmp]
+                else:
+                    return []
+            except:
+                return []
+        return list(current_dic)
