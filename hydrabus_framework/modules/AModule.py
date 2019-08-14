9022ec61b39fa588bd4bc7b2f06a9c7b66fcaa56
ghecko <ovre.jordan@gmail.com>
2019-08-14 16:09:14 +0200

    Add args validator

--- hydrabus_framework/modules/AModule.py
+++ hydrabus_framework/modules/AModule.py
@@ -1,5 +1,6 @@
 from abc import ABC, abstractmethod
-from tabulate import tabulate
+
+from hydrabus_framework.utils.logger import Logger
 
 
 __author__ = "Jordan Ovrè <ghecko78@gmail.com>"
@@ -14,38 +15,66 @@ class AModule(ABC):
             'description': '',
             'author': ''
         }
+        self.logger = Logger()
         self.options = []
 
     def __name__(self):
         return self.name
 
+    def _args_validator(self):
+        """
+        Check arguments type validity & Convert to the specified format
+        :return: Bool
+        """
+        for option in self.options:
+            try:
+                if option["Type"] == "int":
+                    if not isinstance(option["Value"], int):
+                        option["Value"] = int(option["Value"], 10)
+                if option["Type"] == "bool":
+                    if not isinstance(option["Value"], bool):
+                        if str(option["Value"]).upper() == "FALSE":
+                            option["Value"] = False
+                        elif str(option["Value"]).upper() == "TRUE":
+                            option["Value"] = True
+                        else:
+                            raise ValueError
+                if option["Value"] == "None":
+                    option["Value"] = None
+            except ValueError:
+                self.logger.handle("Value error: {} is not a member of {}".format(option["Name"], option["Type"]))
+                return False
+        return True
+
     def check_args(self):
         """
         Check if all arguments are defined by user, or set default value if available
-        TODO: check type
-        :return: Bool, err
+        :return: Bool
         """
         if len(self.options) > 0:
             for option in self.options:
                 if option["Required"] and option["Value"] == "":
                     if option["Default"] == "":
-                        err = "OptionValidateError: The following options failed to validate: {}."\
-                            .format(option["name"])
-                        return False, err
+                        self.logger.handle("OptionValidateError: The following options failed to validate: {}."
+                                           .format(option["name"]), Logger.ERROR)
+                        return False
                     else:
                         option["Value"] = option["Default"]
-        return True, ""
+            if not self._args_validator():
+                return False
+        return True
 
     def get_option_value(self, option_name):
         """
         Return the value of a specific option
         :param option_name: The needed option name
-        :return: Bool, Value
+        :return: Value
         """
         for option in self.options:
             if option["Name"] == option_name:
-                return True, option["Value"]
-        return False, ""
+                return option["Value"]
+        else:
+            raise UserWarning("Value {} not found in module options".format(option_name))
 
     def show_options(self):
         """
@@ -74,13 +103,13 @@ class AModule(ABC):
                             'Description': option["Description"]
                         }
                     )
-            print("\n{}\n".format(tabulate(formatted_options, headers={"Name": "Name", "Description": "Description",
-                                                                       "Value": "Value", "Required": "Required"})))
+            self.logger.print_tabulate(formatted_options, headers={"Name": "Name", "Description": "Description",
+                                                                   "Value": "Value", "Required": "Required"})
 
     def print_meta(self):
-        print('Author: {}'.format(self.meta['author']))
-        print('Module name: {}, version {}'.format(self.meta['name'], self.meta['version']))
-        print('Description: {}'.format(self.meta['description']))
+        self.logger.handle('Author: {}'.format(self.meta['author']), Logger.HEADER)
+        self.logger.handle('Module name: {}, version {}'.format(self.meta['name'], self.meta['version']), Logger.HEADER)
+        self.logger.handle('Description: {}'.format(self.meta['description']), Logger.HEADER)
 
     @abstractmethod
     def run(self):
