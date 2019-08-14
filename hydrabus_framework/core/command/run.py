f45f6e1cadefbc2e6d4e1615e4dee7072a51c2f5
ghecko <ovre.jordan@gmail.com>
2019-08-14 16:08:40 +0200

    modules get options value method change

--- hydrabus_framework/core/command/run.py
+++ hydrabus_framework/core/command/run.py
@@ -14,11 +14,10 @@ def run_module(hbf_instance):
     :return: Nothing
     """
     if isinstance(hbf_instance.current_module, AModule):
-        ret, err = hbf_instance.current_module.check_args()
-        if not ret:
-            hbf_instance.logger.handle(err, Logger.ERROR)
-        try:
-            hbf_instance.current_module.run()
-        except:
-            hbf_instance.logger.handle("Error running module", Logger.ERROR)
-            traceback.print_exc()
+        ret = hbf_instance.current_module.check_args()
+        if ret:
+            try:
+                hbf_instance.current_module.run()
+            except:
+                hbf_instance.logger.handle("Error running module", Logger.ERROR)
+                traceback.print_exc()
